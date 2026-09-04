984a74a953f3c3a5c6e4a680c55442edb0946aa2
Loyalty <132184326+loyaltyLink-s@users.noreply.github.com>
2026-09-05 06:00:55 +0700

    Change APPS_SCRIPT_URL in config.js
    Updated APPS_SCRIPT_URL to a new endpoint.

--- js/config.js
+++ js/config.js
@@ -4,5 +4,5 @@
 const CONFIG = {
   SUPABASE_URL: "https://jjvariwckcbsamfgzeno.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_we79MaWmkimqkDUI-QDl6Q_rH3Mk_OR",
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxQKGhAU_SY6pwcduZ7hYrYujS14WSSPDvSG16mqQOxcmJu68WYpOREWCfjeWuXAt4LAg/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxOyVqni65pcA4FTUhtKL3LG89fUoiSS5TYgZD3enDXs2uKJQaFbQVSGasAdxKhV_dbWg/exec",
 };
