08c422b638e684c713dd1396f90a47c029a20b3c
Loyalty <132184326+loyaltyLink-s@users.noreply.github.com>
2026-09-05 12:13:40 +0700

    Update APPS_SCRIPT_URL in config.js

--- js/config.js
+++ js/config.js
@@ -4,5 +4,5 @@
 const CONFIG = {
   SUPABASE_URL: "https://jjvariwckcbsamfgzeno.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_we79MaWmkimqkDUI-QDl6Q_rH3Mk_OR",
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxOyVqni65pcA4FTUhtKL3LG89fUoiSS5TYgZD3enDXs2uKJQaFbQVSGasAdxKhV_dbWg/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx5xJEZ17JxU0FCKRzCiUsT0uVYCIAcEQyxu5K-SRMTE-tQ0SimB8F2wRLHqgph7nmvwA/exec",
 };
