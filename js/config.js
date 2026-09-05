703b698aba84eacb0072d3067c69f94e427275c9
Loyalty <132184326+loyaltyLink-s@users.noreply.github.com>
2026-09-05 17:31:27 +0700

    Change APPS_SCRIPT_URL to new Google Apps Script
    Updated APPS_SCRIPT_URL to a new endpoint.

--- js/config.js
+++ js/config.js
@@ -4,5 +4,5 @@
 const CONFIG = {
   SUPABASE_URL: "https://jjvariwckcbsamfgzeno.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_we79MaWmkimqkDUI-QDl6Q_rH3Mk_OR",
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwbKTwu32cs694i4rBKel-38CHFVT7L3fC3up4P1W5US9VS49rIG8daLyP1H_OIxRJwsw/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxkkEcZmNvHaIKY3NO2aAsjkv9HnyEAWqTcrq2r_9C9CNRrmpjAou96s_FxZoRUVugDIQ/exec",
 };
