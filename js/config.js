ab260cd70ab615330a1667ffaad8f2c74a1e2ffb
Loyalty <132184326+loyaltyLink-s@users.noreply.github.com>
2026-09-05 23:53:16 +0700

    Remove TURNSTILE_SITE_KEY from config

--- js/config.js
+++ js/config.js
@@ -5,5 +5,4 @@ const CONFIG = {
   SUPABASE_URL: "https://jjvariwckcbsamfgzeno.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_we79MaWmkimqkDUI-QDl6Q_rH3Mk_OR",
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwkqyqTyrLyrYAjQ7ZqNjcAEyRE7P2QKPoVriLyNDzwRDDjbKQUXlpwN3BhluU6V9bM/exec",
-  TURNSTILE_SITE_KEY: "0x4AAAAAAEpUbSOk_LAQE4zk", // aman/publik, boleh terlihat di frontend
 };
