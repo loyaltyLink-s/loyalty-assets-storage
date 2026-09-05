546e1a58d26df3947ed5c85a4d88bed2d9f474e1
Loyalty <132184326+loyaltyLink-s@users.noreply.github.com>
2026-09-05 23:19:23 +0700

    Update APPS_SCRIPT_URL and add TURNSTILE_SITE_KEY

--- js/config.js
+++ js/config.js
@@ -4,5 +4,6 @@
 const CONFIG = {
   SUPABASE_URL: "https://jjvariwckcbsamfgzeno.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_we79MaWmkimqkDUI-QDl6Q_rH3Mk_OR",
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwkqyqTyrLyrYAjQ7ZqNjcAEyRE7P2QKPoVriLyNDzwRDDjbKQUXlpwN3BhluU6V9bM/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw-fKmKn8D4zBSWCZLMP1eWYozkl0jaaI8czo5CWYISDWNrwwP5P10i-zLemnKfvHUu6Q/exec",
+  TURNSTILE_SITE_KEY: "ISI_SITE_KEY_DARI_CLOUDFLARE_DI_SINI", // aman/publik, boleh terlihat di frontend
 };
