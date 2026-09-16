6ea8aa64341cb798da610c12a4dd12be7e9aca58
Loyalty <132184326+loyaltyLink-s@users.noreply.github.com>
2026-09-16 15:34:05 +0700

    Update APPS_SCRIPT_URL in config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
   SUPABASE_URL: "https://jjvariwckcbsamfgzeno.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_we79MaWmkimqkDUI-QDl6Q_rH3Mk_OR",
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzMEvcc8d3-dVUKI4xxGgM2PoPL4tb8r0U61AuGw4jDQDQUz5EHHqmo0NCD96bVo-YCRA/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzYVo3rgAWILevHSWniS8q66C6zAWHDPJPosHr0q_W59Y92c7_4WgYciCZsjFnBNCthbQ/exec",
   // API key Google Cloud (dibatasi cuma buat Google Drive API + domain situs ini),
   // dipakai buat streaming video/audio langsung dari Drive (dukung seek asli)
   GOOGLE_API_KEY: "AIzaSyCt1Z93jdVX_hLq-NQT0myPLoA-S8sjSU4",
